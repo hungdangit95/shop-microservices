0e52186008a7f93b49fb6cb686ca2eebb1d3bd39
hungdangit95 <hungdangit95@gmail.com>
2022-12-18 22:42:53 +0700

    first init

---
+++ src/Services/Product.API/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Product.API.Extensions
+{
+    public static class ApplicationExtentions
+    {
+        public static void UseInfrastructure(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                //c.OAuthClientId("tedu_microservices_swagger");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API");
+                c.DisplayRequestDuration();
+            });
+          //  app.UseMiddleware<ErrorWrappingMiddleware>();
+           // app.UseAuthentication();
+
+            app.UseRouting();
+            //app.UseHttpsRedirection();
+
+           // app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                   // ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
