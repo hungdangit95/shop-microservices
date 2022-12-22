ac16513d8b097e290711d600c2348b6e315623c2
hungdangit95 <hungdangit95@gmail.com>
2022-12-22 23:34:41 +0700

    implement custom api

--- src/Services/Customer.API/Program.cs
+++ src/Services/Customer.API/Program.cs
@@ -1,46 +1,56 @@
-
-using Common.Logging;
 using Serilog;
+using Customer.API.Extensions;
+using Customer.API.Persistence;
 
-Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
-Log.Information("Starting Customer API up");
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
+var builder = WebApplication.CreateBuilder(args);
+Log.Information($"Start {builder.Environment.ApplicationName} Api up");
 try
 {
-    var builder = WebApplication.CreateBuilder(args);
-
-    builder.Host.UseSerilog(Serilogger.Configure);
-    // Add services to the container.
-
     builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+    builder.Host.AddAppConfigurations();
+
+    builder.Services.ConfigureServices(builder.Configuration);
 
     var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
         app.UseSwaggerUI();
     }
 
-    app.UseHttpsRedirection();
+    app.UseRouting();
+    //app.UseHttpsRedirection();
 
     app.UseAuthorization();
+    app.UseEndpoints(endpoints =>
+    {
+        //endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+        //{
+        //    Predicate = _ => true,
+        //    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        //});
+        endpoints.MapDefaultControllerRoute();
+    });
 
-    app.MapControllers();
-
+    //app.SeedCustomer().Run();
     app.Run();
 }
 catch (Exception ex)
 {
-    Log.Fatal(ex, "Unhandlerd exception");
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+        throw;
+    Log.Fatal(ex, "Unhanded exception");
 }
 finally
 {
     Log.Information("Shut down Customer API complete");
     Log.CloseAndFlush();
 }
-
